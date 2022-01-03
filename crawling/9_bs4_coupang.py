b8ea8c7dea165be4424efa5e62448779eb2f1d7c
SangHwan Jeong <yajang12@gamil.com>
2022-01-03 22:50:44 +0900

    24th commit

---
+++ crawling/9_bs4_coupang.py
@@ -0,0 +1,14 @@
+# 출처: https://www.youtube.com/watch?v=yQ20jZwDjTE&t=1035s&ab_channel=%EB%82%98%EB%8F%84%EC%BD%94%EB%94%A9
+
+import requests
+import re
+from bs4 import BeautifulSoup
+
+url = "https://www.coupang.com/np/search?component=&q=%EB%85%B8%ED%8A%B8%EB%B6%81&channel=user"
+headers = {"User-Agent":"Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/96.0.4664.110 Safari/537.36"}
+res = requests.get(url, headers=headers)
+res.raise_for_status()
+soup = BeautifulSoup(res.text, "lxml")
+
+items = soup.find_all("li", attrs={"class":re.compile("^search-product")})
+print(items[0].find("div", attrs={"class":"name"}).get_text())
