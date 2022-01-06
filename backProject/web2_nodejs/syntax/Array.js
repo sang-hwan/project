380028dcd513175ebeefa22472ffbeb94dfa4fd9
SangHwan Jeong <yajang12@gamil.com>
2022-01-06 22:33:12 +0900

    29th commit

---
+++ backProject/web2_nodejs/syntax/Array.js
@@ -0,0 +1,10 @@
+// 출처: https://www.youtube.com/watch?v=FvOKF4pyXnM&list=PLuHgQVnccGMA9QQX5wqj6ThK7t2tsGxjm&index=27&ab_channel=%EC%83%9D%ED%99%9C%EC%BD%94%EB%94%A9
+
+var arr = ['A', 'B', 'C', 'D'];
+console.log(arr[1]);
+console.log(arr[3]);
+arr[2] = 3;
+console.log(arr);
+console.log(arr.length);
+arr.push('E');
+console.log(arr);
---
+++ backProject/web2_nodejs/syntax/Loop.js
@@ -0,0 +1,13 @@
+// 출처: https://www.youtube.com/watch?v=FvOKF4pyXnM&list=PLuHgQVnccGMA9QQX5wqj6ThK7t2tsGxjm&index=27&ab_channel=%EC%83%9D%ED%99%9C%EC%BD%94%EB%94%A9
+
+console.log('A');
+console.log('B');
+
+var i = 0;
+while(i < 2){
+    console.log('C1');
+    console.log('C2');
+    i = i + 1;
+}
+
+console.log('D');
