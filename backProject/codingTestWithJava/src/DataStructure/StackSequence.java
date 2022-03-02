9f2df06b306fb8662b2904b044a200cb2465a8d1
sanghwan <yajang12@gmail.com>
2022-03-02 23:41:37 +0900

    58th commit

--- backProject/codingTestWithJava/src/DataStructure/StackSequence.java
+++ backProject/codingTestWithJava/src/DataStructure/StackSequence.java
@@ -3,33 +3,38 @@ package DataStructure;
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
-import java.util.ArrayList;
 import java.util.Stack;
 
 public class StackSequence {
     public static void main(String[] args) throws IOException{
+        // 출처:
+        // https://www.acmicpc.net/problem/1874
+        // https://st-lab.tistory.com/182
+
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         StringBuilder sb = new StringBuilder();
-        int n = Integer.parseInt(br.readLine());
+
         Stack stack = new Stack();
-        ArrayList check = new ArrayList<>();
-        for(int i=1; i<=n; i++){
-            check.add(i);
-        }
-        for(int j=1; j<=n; j++){
+
+        int n = Integer.parseInt(br.readLine());
+
+        int start = 0;
+
+        while(n-- > 0){
             int value = Integer.parseInt(br.readLine());
-            if(check.contains(value)){
-                for(int k=(int)check.get(0); k<=value; k++){
-                    stack.push(k);
-                    check.remove(k);
+
+            if(value>start){
+                for(int i=start+1; i<=value; i++){
+                    stack.push(i);
                     sb.append("+\n");
                 }
-                stack.pop();
-                sb.append("-\n");
-            }else{
-                stack.pop();
-                sb.append("-\n");
+                start = value;
+            }else if((int)stack.peek()!=value){
+                System.out.println("NO");
+                return;
             }
+            stack.pop();
+            sb.append("-\n");
         }
         System.out.println(sb);
     }
