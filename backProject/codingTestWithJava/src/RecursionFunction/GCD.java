52f2a50d64ae0c3a610f6ccfa8cc9c4212d147d0
sanghwan <yajang12@gmail.com>
2022-03-15 19:05:05 +0900

    63th commit

--- backProject/codingTestWithJava/src/RecursionFunction/GCD.java
+++ backProject/codingTestWithJava/src/RecursionFunction/GCD.java
@@ -6,7 +6,7 @@ import java.io.InputStreamReader;
 import java.io.OutputStreamWriter;
 import java.util.StringTokenizer;
 
-public class GreatestCommonFactor {
+public class GCD {
 
     static BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
     static BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
