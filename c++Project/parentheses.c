6413bab8b2d2bce1248e9cbe5cb9ff30fd3a0cb3
sanghwan <yajang12@gmail.com>
2022-04-09 22:56:41 +0900

    86th commit

---
+++ c++Project/parentheses.c
@@ -0,0 +1,48 @@
+#include<stdio.h>
+#include<stdbool.h>
+#define max_size 100
+typedef struct Stacktype{
+    int stack[max_size];
+    int top;
+}Stacktype;
+
+void Stack_init(Stacktype*s){
+    s->top = -1;
+}
+
+void Stack_push(Stacktype*s, int item){
+    s->stack[++(s->top)] = item;
+    return;
+}
+
+bool is_empty(Stacktype*s){
+    return s->top == -1;
+}
+
+int Stack_pop(Stacktype*s){
+    int t = s->stack[s->top];
+    s->stack[s->top] = 0;
+    s->top--;
+    return t;
+}
+
+int main(void){
+    Stacktype s;
+    Stack_init(&s);
+    char input[max_size] = {};
+    printf("수식 : ");
+    scanf_s("%s", input, sizeof(input));
+    printf("괄호 수 : ");
+    int i = 0, count = 1;
+    while(input[i] != NULL){
+        if(input[i] == '('){
+            Stack_push(&s, count);
+            printf("%d", count);
+            count++;
+        }else{
+            printf("%d", Stack_pop(&s));
+        }
+        i++;
+    }
+    return 0;
+}
